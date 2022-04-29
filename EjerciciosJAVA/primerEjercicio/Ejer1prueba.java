4b08b8ef7a36dec50da5472ff9391b278b8aac2f
Andrés <79253656+AndresOlmedo27@users.noreply.github.com>
2022-04-29 12:41:06 +0200

    Add files via upload

---
+++ EjerciciosJAVA/primerEjercicio/Ejer1prueba.java
@@ -0,0 +1,92 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Ejer1prueba {
+ 
+    public static void main(String arg[]) {
+
+        // String nombre1;
+        boolean validar = false; 
+        String apellido1;
+        String apellido2;
+        String regEx =  "[^a-zA-Z]+";//"[(\\d)(\\p{P}\\p{S})(^\\s*)]+";
+        Scanner name = new Scanner(System.in);
+        Scanner surn1 = new Scanner(System.in);
+        Scanner surn2 = new Scanner(System.in);
+        
+        while (!validar) {
+            try{
+            System.out.println("Por favor, introduce tu nombre: ");
+            String nombre = name.nextLine();
+            Pattern patron = Pattern.compile(regEx);
+            Matcher busqueda = patron.matcher(nombre);
+            
+            if (busqueda.find()) {
+                System.out.println("Formato inválido");
+
+            } else {
+                System.out.println("Hola " + nombre + " :)");
+                System.out.println("Este es tu nombre en minúsculas: " + nombre.toLowerCase());
+                System.out.println("Este es tu nombre en mayúsculas: " + nombre.toUpperCase());
+                System.out.println("Ahora se pondrá la primera letra de tu nombre en mayúscula: "
+                        + nombre.substring(0, 1).toUpperCase() + nombre.substring(1).toLowerCase());
+                validar = true;
+
+            }
+            }catch(StringIndexOutOfBoundsException ex){
+                System.out.println("Introduce un formato válido");
+            }
+        
+        }
+      
+
+        while (validar) {
+            try{
+            System.out.println("Por favor, introduce tu primer apellido: ");
+            apellido1 = surn1.nextLine();
+            Pattern patron2 = Pattern.compile(regEx);
+            Matcher busqueda2 = patron2.matcher(apellido1);
+
+            if (busqueda2.find()) {
+                System.out.println("Formato inválido");
+
+            } else {
+                System.out.println("¡Genial! :)");
+                System.out.println("Este es tu primer apellido en minúsculas: " + apellido1.toLowerCase());
+                System.out.println("Este es tu primer apellido en mayúsculas: " + apellido1.toUpperCase());
+                System.out.println("Ahora se pondrá la primera letra de tu primer apellido en mayúscula: "
+                        + apellido1.substring(0, 1).toUpperCase() + apellido1.substring(1).toLowerCase());
+                validar = false;
+            }
+            }catch(StringIndexOutOfBoundsException ex){
+                System.out.println("Introduce un formato válido");
+            }
+
+        }
+        while (!validar) {
+            try{
+
+            System.out.println("Por favor, ahora introduce tu segundo apellido: ");
+            apellido2 = surn2.nextLine();
+            Pattern patron3 = Pattern.compile(regEx);
+            Matcher busqueda3 = patron3.matcher(apellido2);
+
+            if (busqueda3.find()) {
+                System.out.println("Formato inválido");
+
+            } else {
+                System.out.println("Este es tu segundo apellido en minúsculas: " + apellido2.toLowerCase());
+                System.out.println("Este es tu segundo apellido en mayúsculas: " + apellido2.toUpperCase());
+                System.out.println("Ahora se pondrá la primera letra de tu segundo apellido en mayúscula: "
+                        + apellido2.substring(0, 1).toUpperCase() + apellido2.substring(1).toLowerCase());
+                validar = true;
+            }
+            }catch(StringIndexOutOfBoundsException ex){
+                System.out.println("Introduce un formato válido");
+            }
+        }
+       
+    }
+        
+}
